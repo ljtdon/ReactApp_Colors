6024fbba07b168b7d62f3954f37c86f9c0b80505
Ljilja <ljiljana.tarabaric@gmail.com>
2018-03-19 18:02:54 +0100

    new comp

---
+++ src/components/BlazingComp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class BlazingComp extend Component {
+
+	render() {
+		return (
+		<div>
+			<h1>New Comp BlazingComp</h1>
+		</div>
+		)
+	}
+
+}
+
+export default BlazingComp;
