8513493794f70859fff6a01c55acc7add76c3d9e
Ljilja <ljiljana.tarabaric@gmail.com>
2018-03-21 14:41:32 +0100

    update toggle

--- src/App.js
+++ src/App.js
@@ -9,56 +9,80 @@ class App extends Component {
   state = {
     content: 'Text from Component',
     style: {
-      color: 'red'
-    }   
+      color: '#000'
+    },
+    switcher: false
   }
 
+
+  // Input bind to Component text
   changeContent = (event) => {
     this.setState({
-      content: event.target.value
+      content: event.target.value      
     });
   }
- 
 
 
+  // Changing color
   changeStyle = () => {
-    fetch('http://www.colr.org/json/color/random')     
-        .then(res => res.json())         
-        .then(data => this.getColor(data.colors[0].hex));
 
+    this.switcher = !this.switcher;
+    
+    if(this.switcher){
 
-    this.getColor = (color) => {
-        color = '#'.concat(color.toString());
-        console.log(color);
+      // Fetch color
+      fetch('http://www.colr.org/json/color/random')     
+      .then(res => res.json())         
+      .then(data => this.getColor(data.colors[0].hex));
 
-        this.setState({
-          content: ' Fetched content',
-          style: {      
-            backgroundColor: color            
-          }    
-        });
-     }    
+      // Adjusting color and change state
+      this.getColor = (color) => {
+          if(color==='') {
+            console.log(color);
+            this.changeStyle();
+          }
+          else {            
+            color = '#'.concat(color.toString());
+            console.log(color);
+            this.setState({          
+              style: {      
+                color: color            
+            }    
+          });
+          }
+         
+      } 
+    }      
+    else {
+      this.setState({          
+        style: {      
+          color: '#000'           
+        }    
+      });
+    } 
+    
+    
   }
-  
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
 
 
-         <BlazingComp 
-           content={this.state.content}
-           style={this.state.style}           
-           click={this.changeStyle}></BlazingComp>
-          
-          <input type="text" onChange={this.changeContent} />
-      </div>
+render() {
+  return (
+    <div className="App">
+    <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1 className="App-title">Welcome to React</h1>
+    </header>
+
+
+    <BlazingComp 
+    content={this.state.content}
+    style={this.state.style}           
+    click={this.changeStyle}></BlazingComp>
+
+    <input type="text" onChange={this.changeContent} />
+    </div>
     );
-  }
+}
 }
 
 export default App;
